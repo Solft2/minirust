a9793fe87d03f3b632e6a4d71438e2b7392f2e7d
Ian Gabriel <50637958+IanGabriel12@users.noreply.github.com>
2025-11-28 02:38:50 -0300

    Page minigit log command

--- src/commands/log.rs
+++ src/commands/log.rs
@@ -1,4 +1,6 @@
 use chrono::{DateTime, Local, TimeZone};
+use std::process::{Command, Stdio};
+use std::io::Write;
 
 use crate::{objects::RGitObject, utils::find_current_repo};
 
@@ -12,6 +14,7 @@ pub fn cmd_log() {
 fn cmd_log_result() -> Result<(), String> {
     let repo = find_current_repo().ok_or("Não está dentro de um repositório")?;
     let commits = repo.get_commit_history();
+    let mut output = String::new();
 
     if commits.is_empty() {
         println!("Nenhum commit encontrado no repositório.");
@@ -22,12 +25,30 @@ fn cmd_log_result() -> Result<(), String> {
         let dt: DateTime<Local> = Local.timestamp_opt(commit.timestamp as i64, 0).unwrap();
         let formatted_date = dt.format("%d/%m/%Y %H:%M:%S").to_string();
 
-        println!("commit {}", commit.hash());
-        println!("Autor: {}", commit.author);
-        println!("Data:  {}", formatted_date);
-        println!();
-        println!("\t{}", commit.message);
-        println!();
+        output.push_str(&format!("commit {}\n", commit.hash()));
+        output.push_str(&format!("Autor: {}\n", commit.author));
+        output.push_str(&format!("Data:  {}\n", formatted_date));
+        output.push_str("\n");
+        output.push_str(&format!("\t{}\n", commit.message));
+        output.push_str("\n");
     }
+
+    run_with_pager(&output);
     Ok(())
+}
+
+/// Executa o comando 'less' para paginar a saída
+/// 
+/// Depende que o `less` esteja instalado no sistema
+/// 
+/// ## Arguments
+/// * `output` - A string que será passada para o pager
+fn run_with_pager(output: &str) {
+    let mut child = Command::new("less")
+        .stdin(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    child.stdin.as_mut().unwrap().write_all(output.as_bytes()).unwrap();
+    child.wait().unwrap();
 }
